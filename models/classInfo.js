7c13ed25f5f65e5fff51e84b3622c8779625c929
Sean R Reid <sean@torchcodelab.com>
2019-10-21 15:55:25 -0400

    Cleaned up bug with the redirect not getting current data

--- models/classInfo.js
+++ models/classInfo.js
@@ -1,7 +1,7 @@
-const db = require('./conn.js');
+const db = require("./conn.js");
 
 class ClassInfo {
-    constructor(id, name, rank){
+    constructor(id, name, rank) {
         this.id = id;
         this.name = name;
         this.rank = rank;
@@ -19,7 +19,7 @@ class ClassInfo {
                     class_status on topics.status_id = class_status.id 
                 ORDER BY topics.id;`);
             return response;
-        } catch(err) {
+        } catch (err) {
             return err.message;
         }
     }
@@ -28,18 +28,17 @@ class ClassInfo {
         try {
             const response = await db.any(`SELECT * FROM class_status;`);
             return response;
-        } catch(err) {
+        } catch (err) {
             return err.message;
         }
     }
 
     static async update(topic, rank) {
-        const query = `UPDATE topics SET status_id=${rank} WHERE topic_name = '${topic}'`
+        const query = `UPDATE topics SET status_id=${rank} WHERE topic_name = '${topic}'`;
         try {
             const response = await db.result(query);
-            console.log("response is", response);
             return response;
-        } catch(err) {
+        } catch (err) {
             console.log("ERROR", err.message);
             return err;
         }
--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const ClassDataModel = require('../models/classInfo');
+const ClassDataModel = require("../models/classInfo");
 
-const renderItems = async (res, topicData, topicStatusList) => {
-  return res.render('template', { 
-    locals: {
-      title: 'Class Topic Ranking',
-      topicData: topicData,
-      topicStatusList: topicStatusList
-    },
-    partials: {
-      partial: 'partial-index'
-    }
-  });
-}
+const updateEach = async items => {
+    const entriesArray = Object.keys(items);
 
-const updateEach = async (items) => {
-  for(let key in items) {
-    ClassDataModel.update(key, items[key]);
-  }
-}
+    const results = await Promise.all(
+        entriesArray.map(key => {
+            return ClassDataModel.update(key, items[key]);
+        })
+    );
+    return results;
+};
 
 /* GET home page. */
-router.get('/', async (req, res, next) => {
-  const topicData = await ClassDataModel.getAllTopicData();
-  const topicStatusList = await ClassDataModel.getAllClassStatus();
+router.get("/", async (req, res, next) => {
+    const topicData = await ClassDataModel.getAllTopicData();
+    const topicStatusList = await ClassDataModel.getAllClassStatus();
 
-  renderItems(res, topicData, topicStatusList);
+    res.render("template", {
+        locals: {
+            title: "Class Topic Ranking",
+            topicData: topicData,
+            topicStatusList: topicStatusList
+        },
+        partials: {
+            partial: "partial-index"
+        }
+    });
 });
 
-router.post('/', async (req, res) => {
-  const addUpdatedValues = await updateEach(req.body);
-  const topicData = await ClassDataModel.getAllTopicData();
-  const topicStatusList = await ClassDataModel.getAllClassStatus();
+router.post("/", async (req, res) => {
+    const addUpdatedValues = await updateEach(req.body);
 
-  renderItems(res, topicData, topicStatusList);
+    if (addUpdatedValues) {
+        res.status(200).redirect("/");
+    } else {
+        res.sendStatus(500);
+    }
 });
 
 module.exports = router;
